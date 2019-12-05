811e461f7399f8828beb2c103983cfc659be1c93
ChrisWang10 <wchao118@gmail.com>
2019-12-05 10:14:22 -0500

    find projection

---
+++ LDA.py
@@ -0,0 +1,55 @@
+# the main idea of LDA is find a projection that
+# can maximize inter-class variance and minimize intra-class distance
+import pandas as pd
+import numpy as np
+import os
+from sklearn.preprocessing import LabelBinarizer
+from sklearn.utils.multiclass import unique_labels
+
+train_data_path = './data/cancer-data-train.csv'
+test_data_path = './data/cancer-data-test.csv'
+
+
+class LDA:
+    def __init__(self, train, test):
+        self.train = train
+        self.test = test
+        self.unique_classes = list(set(train['label']))
+        self.lda_fit()
+
+    def lda_fit(self):
+        specific_subsets = []
+        centered_subsets = []
+        u = []
+        for specific_class in self.unique_classes:
+            specific_subset = self.train[self.train['label'] == specific_class].iloc[:, :-1]
+            specific_subsets.append(specific_subset)
+            u.append(np.mean(specific_subset, axis=0))
+
+
+
+
+
+def preprocess_data():
+    """
+    :return: convert category to binary
+    """
+    lb = LabelBinarizer()
+    trainData, testData = pd.read_csv(train_data_path, header=None), pd.read_csv(test_data_path, header=None)
+    X_train, Y_train = trainData.iloc[:, :-1], trainData.iloc[:, -1]
+    X_test, Y_test = testData.iloc[:, :-1], testData.iloc[:, -1]
+    Y_train_lb, Y_test_lb = lb.fit_transform(Y_train), lb.fit_transform(Y_test)
+    X_train['label'] = pd.Series(np.squeeze(Y_train_lb))
+    X_test['label'] = pd.Series(np.squeeze(Y_test_lb))
+
+    return X_train, X_test
+
+
+def main():
+    train, test = preprocess_data()
+    LDA(train, test)
+    # shape of train (2,) train[0] is data and train[1] is label
+
+
+if __name__ == '__main__':
+    main()

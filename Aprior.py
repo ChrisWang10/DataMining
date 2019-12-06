dd1aa8b0f350734bb53de153aca4849f8f643b65
ChrisWang10 <wchao118@gmail.com>
2019-12-05 21:10:13 -0500

    frequent itemsets

--- Aprior.py
+++ Aprior.py
@@ -10,12 +10,30 @@ transactions = [
     [1, 2, 3]
 ]
 
+
+def check_frequency(candidate_set, min_sup):
+    count = 0
+    for transaction in transactions:
+        if candidate_set.issubset(frozenset(transaction)):
+            count += 1
+        if count >= min_sup:
+            return True
+    return False
+
+
 def get_frequent_item_sets(frequent_item_sets):
     # get candidates from previous FIS(frequent item sets)
-
-
-
-
+    candidates = []
+    record = []
+    for i in range(len(frequent_item_sets)):
+        for j in range(i, len(frequent_item_sets)):
+            union_sets = frequent_item_sets[i].union(frequent_item_sets[j])
+            if union_sets not in record:
+                record.append(union_sets)
+            else:
+                continue
+            if len(union_sets) == len(frequent_item_sets) + 1:
+                candidates.append(union_sets)
 
 
 def main():
@@ -26,11 +44,9 @@ def main():
     for v in candidate:
         for transaction in transactions:
             if v in transaction:
-                counter[v] = 1 if v not in counter.keys() else counter[v]+1
+                counter[v] = 1 if v not in counter.keys() else counter[v] + 1
         if counter[v] > min_support:
-            frequent_itemsets.append(v)
-
-
+            frequent_itemsets.append(frozenset([v]))
 
 
 if __name__ == '__main__':
